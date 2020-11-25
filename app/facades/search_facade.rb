1abe751df590356b1969ad7c8f407103f4da3124
Corey Carter <crc2231@gmail.com>
2020-11-24 17:37:37 -0700

    remove pry

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -2,11 +2,9 @@ class SearchFacade
   def self.find(location)
     json = SearchService.find_station(location)
     @station = Station.new(json[:fuel_stations][0])
-    binding.pry
   end
 
   def self.get(from, address, city, state)
     json = MapService.get_directions(address, city, state)
-    binding.pry
   end
 end
